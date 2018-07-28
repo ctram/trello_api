89f33db91155d17a1301c7f285157c45a465c532
Chris Tram <chris.tram@gmail.com>
2018-07-28 14:42:27 -0700

    update check authorization file name

--- spec/shared/examples/check_authorization.rb
+++ spec/shared/examples/check_authorization.rb
@@ -1,4 +1,4 @@
-RSpec.shared_examples 'check authentication' do |request_type, path, options|
+RSpec.shared_examples 'check authorization' do |request_type, path, options|
   it 'receives a 401 unless authenticated' do
     method(request_type).call(path, options)
     expect(response.status).to be 401
