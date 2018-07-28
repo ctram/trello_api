b47c59580632f550f45a326d1eec10a5b8627114
Chris Tram <chris.tram@gmail.com>
2018-07-27 18:10:43 -0700

    adds bearer token authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+  before_action :check_authorization
+  
+  AUTHORIZED_API_TOKENS = %w[ehzLoAaX7hVUxJ2D3vLkxQ].freeze # ehzLoAaX7hVUxJ2D3vLkxQ
+
+  def check_authorization
+    render status: 401 unless authorized?
+  end
+
+  def authorized?
+    AUTHORIZED_API_TOKENS.include? bearer_token
+  end
+
+  def bearer_token
+    request.headers['Authorization'] && request.headers['Authorization'].split(' ').last
+  end
 end
