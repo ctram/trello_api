72b1a89dee320c02f90b586353268c6c9185e101
Chris Tram <chris.tram@gmail.com>
2018-07-28 15:28:07 -0700

    refactor authorization headers shared context

--- spec/shared/contexts/authorization_headers.rb
+++ spec/shared/contexts/authorization_headers.rb
@@ -1,5 +1,5 @@
-
 RSpec.shared_context 'authorization headers' do
-
-  let(:authorization_headers) { { 'Authorization' => 'Bearer ehzLoAaX7hVUxJ2D3vLkxQ' } }
+  let(:authorization_headers) do 
+    { 'Authorization' => 'Bearer ehzLoAaX7hVUxJ2D3vLkxQ' } 
+  end
 end
