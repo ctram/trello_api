c71a20651d8559f86e6e9007c451c7cf947667f9
Chris Tram <chris.tram@gmail.com>
2018-07-28 15:27:51 -0700

    request to boards controlle rspecs

--- spec/requests/boards_spec.rb
+++ spec/requests/boards_spec.rb
@@ -21,9 +21,39 @@ RSpec.describe 'Boards', type: :request do
 
   include_context 'authorization headers'
 
-  it 'GET /boards' do
+  it 'GET index' do
     get '/boards', params: {}, headers: authorization_headers
     expect(response).to have_http_status(200)
     expect(JSON.parse(response.body).count).to be 3
+    expect( response.body).to eq(Board.all.to_json)
+  end
+
+  it 'GET show' do
+    get "/boards/#{Board.all.first.id}", params: {}, headers: authorization_headers
+    expect(response.body).to eq(Board.first.to_json)
+  end
+
+  it 'POST create' do
+    post '/boards', params: { board: { title: 'Batman', name: 'Bruce Wayne' } }, headers: authorization_headers
+    expect(response.body).to eq(Board.last.to_json)
+    expect(response).to have_http_status(201)
+    expect(Board.count).to be 4
+  end
+
+  it 'PUT update' do
+    put "/boards/#{Board.all.first.id}", params: { board: { title: 'Updated title', name: 'Updated name' } }, headers: authorization_headers
+    expect(response.body).to eq(Board.first.to_json)
+    expect(response).to have_http_status(200)
+    expect(Board.count).to be 3
+    expect(Board.first.name).to eq('Updated name')
+    expect(Board.first.title).to eq('Updated title')
+  end
+
+  it 'DELETE destroy' do
+    delete "/boards/#{Board.all.first.id}", params: {}, headers: authorization_headers
+    expect(response).to have_http_status(204)
+    expect(response.body).to be_empty
+    expect(Board.count).to be 2
+    expect(Board.where(title: 'One').count).to be 0
   end
 end
