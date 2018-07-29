1d1127a929f65aecf439269fd0af09c2ee056922
Chris Tram <chris.tram@gmail.com>
2018-07-29 15:33:24 -0700

    namespace shared examples so they do not run in parallel

--- spec/requests/columns_spec.rb
+++ spec/requests/columns_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 require_relative '../../app/models/column'
 require_relative '../shared/examples/CRUD_actions'
 require_relative '../shared/examples/update_position'
-require_relative '../shared/contexts/test_objects'
 
 RSpec.describe 'Columns', type: :request do
-  include_examples 'CRUD actions', Column, :board
-  include_examples 'update position', Column, :board
+  describe 'CRUD actions' do
+    include_examples 'CRUD actions', Column, :board
+  end
+  describe 'update position' do
+    include_examples 'update position', Column, :board
+  end
 end
