86d7adb78c78f917243cd48fea4049215284db64
Chris Tram <chris.tram@gmail.com>
2018-07-29 12:51:42 -0700

    adds custom validation for Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,15 +1,18 @@
 class Task < ApplicationRecord
   include Positionable
 
+  validates :position, numericality: { only_integers: true }
+  validate :valid_position
   validates_presence_of :title, :name, :column_id, :position
+
   has_many :comments, dependent: :destroy
   belongs_to :column
 
   before_validation do
     ensure_position_exists
   end
-  
+
   def siblings
-    column.tasks
+    column.tasks.where.not(id: id)
   end
 end
