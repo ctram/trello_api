dc4de995cc677e490934c115ead6ae2683339863
Chris Tram <chris.tram@gmail.com>
2018-07-29 14:27:49 -0700

    update names for CRUD actions and check authorization

--- spec/shared/examples/CRUD_actions.rb
+++ spec/shared/examples/CRUD_actions.rb
@@ -3,30 +3,29 @@ require_relative '../contexts/authorization_headers'
 require_relative '../contexts/test_objects'
 require_relative './check_authorization'
 
-RSpec.shared_examples 'CRUD actions' do |model, parent_name = nil|
+RSpec.shared_examples 'CRUD actions' do |child_class, parent_type = nil|
   include_context 'authorization headers'
   include_context 'test objects'
 
-  let(:model_name) { model.to_s.downcase }
+  let(:model_name) { child_class.to_s.downcase }
   let(:model_name_plural) { model_name + 's' }
   let(:parent) do
     {
       board: board,
       column: column,
       task: task
-    }[parent_name]
+    }[parent_type]
   end
 
   before(:example) do
     %w[one two three].map do |num|
-      FactoryBot.create(model.to_s.downcase.to_sym, title: num, name: num)
+      FactoryBot.create(child_class.to_s.downcase.to_sym, title: num, name: num)
     end
   end
 
-
-  def path1(model, parent = nil)
-    return boards_url if model == Board
-    send("#{parent.class.to_s.downcase}_#{model.to_s.downcase}s_url", parent.id)
+  def path1(child_class, parent = nil)
+    return boards_url if child_class == Board
+    send("#{parent.class.to_s.downcase}_#{child_class.to_s.downcase}s_url", parent.id)
   end
 
   def path2(object)
@@ -34,51 +33,52 @@ RSpec.shared_examples 'CRUD actions' do |model, parent_name = nil|
   end
 
   describe 'check authorization' do
-    include_examples 'check authorization', :index, model
-    include_examples 'check authorization', :create, model
-    include_examples 'check authorization', :show, model
-    include_examples 'check authorization', :update, model
-    include_examples 'check authorization', :destroy, model
+    include_examples 'check authorization', :index, child_class
+    include_examples 'check authorization', :create, child_class
+    include_examples 'check authorization', :show, child_class
+    include_examples 'check authorization', :update, child_class
+    include_examples 'check authorization', :destroy, child_class
   end
 
   it 'GET index' do
-    get path1(model, parent), params: {}, headers: authorization_headers
+    byebug
+    get path1(child_class, parent), params: {}, headers: authorization_headers
     expect(response).to have_http_status(200)
     expect(JSON.parse(response.body).count).to be 4
-    expect(response.body).to eq(model.all.to_json)
+    expect(response.body).to eq(child_class.all.to_json)
   end
 
   it 'GET show' do
-    get path2(model.first), params: {}, headers: authorization_headers
-    expect(response.body).to eq(model.first.to_json)
+    get path2(child_class.first), params: {}, headers: authorization_headers
+    expect(response.body).to eq(child_class.first.to_json)
   end
 
   it 'POST create' do
     params = {}
-    params[model.to_s.downcase] = { title: 'Batman', name: 'Bruce Wayne' }
-    post path1(model, parent), params: params, headers: authorization_headers
-    expect(response.body).to eq(model.last.to_json)
+    params[child_class.to_s.downcase] = { title: 'Batman', name: 'Bruce Wayne' }
+    post path1(child_class, parent), params: params, headers: authorization_headers
+    expect(response.body).to eq(child_class.last.to_json)
     expect(response).to have_http_status(201)
-    expect(model.count).to be 5
+    expect(child_class.count).to be 5
   end
 
   it 'PUT update' do
     params = {}
     params[model_name] = { title: 'Updated title', name: 'Updated name' }
-    put path2(model.first), params: params, headers: authorization_headers
+    put path2(child_class.first), params: params, headers: authorization_headers
     expect(response).to have_http_status(200)
-    expect(model.count).to be 3
+    expect(child_class.count).to be 3
     expect(JSON.parse(response.body)['title']).to eq('Updated title')
     expect(JSON.parse(response.body)['name']).to eq('Updated name')
-    expect(model.first.name).to eq('Updated name')
-    expect(model.first.title).to eq('Updated title')
+    expect(child_class.first.name).to eq('Updated name')
+    expect(child_class.first.title).to eq('Updated title')
   end
 
   it 'DELETE destroy' do
-    delete path2(model.first), params: {}, headers: authorization_headers
+    delete path2(child_class.first), params: {}, headers: authorization_headers
     expect(response).to have_http_status(204)
     expect(response.body).to be_empty
-    expect(model.count).to be 2
-    expect(model.where(title: 'One').count).to be 0
+    expect(child_class.count).to be 2
+    expect(child_class.where(title: 'One').count).to be 0
   end
 end
--- spec/shared/examples/check_authorization.rb
+++ spec/shared/examples/check_authorization.rb
@@ -1,4 +1,4 @@
-RSpec.shared_examples 'check authorization' do |request_action, model|
+RSpec.shared_examples 'check authorization' do |request_action, model_class|
   let(:title) { 'title'.freeze }
   let(:name) { 'name'.freeze }
 
@@ -7,13 +7,13 @@ RSpec.shared_examples 'check authorization' do |request_action, model|
   let(:task) { column.tasks.create(title: title, name: name) }
   let(:comment) { task.comments.create(content: 'hehe') }
 
-  def paths(model)
+  def paths(model_class)
     {
       board: %w[/boards /boards/1],
       column: %w[/boards/1/columns /columns/1],
       task: %w[/columns/1/tasks /tasks/1],
       comment: %w[/tasks/1/comments /comments/1]
-    }[model.to_s.downcase.to_sym]
+    }[model_class.to_s.downcase.to_sym]
   end
 
   def method_verb(request_action)
@@ -27,7 +27,7 @@ RSpec.shared_examples 'check authorization' do |request_action, model|
   end
 
   it "#{request_action} receives a 401 unless authorized" do
-    path = %i[index create].include?(request_action) ? paths(model).first : paths(model).last
+    path = %i[index create].include?(request_action) ? paths(model_class).first : paths(model_class).last
     method(method_verb(request_action)).call(path)
     expect(response.status).to be 401
   end
