8233a0da1a04ab8300e9a4ec6ac5efaaab52960d
Chris Tram <chris.tram@gmail.com>
2018-07-29 00:39:44 -0700

    add transaction block for update_position

--- app/models/concerns/positionable.rb
+++ app/models/concerns/positionable.rb
@@ -1,5 +1,5 @@
 module Positionable
-  def ensure_has_position
+  def ensure_position_exists
     update(position: siblings.length) if position.nil?
   end
 
@@ -7,36 +7,37 @@ module Positionable
     raise 'siblings method must be defined'
   end
 
-  def move_to(new_position)
-    raise ArgumentError, 'Argument must be an integer' unless new_position.is_a?(Integer)
-    orig_position = position
-    num_items = siblings.length
-
-    if new_position < 0
-      new_position = 0
-    elsif new_position > num_items - 1
-      new_position = num_items - 1
+  def update_position(new_position)
+    ActionRecord::Base.transaction do
+      raise ArgumentError, 'Argument must be an integer' unless new_position.is_a?(Integer)
+      orig_position = position
+      num_items = siblings.length
+  
+      if new_position < 0
+        new_position = 0
+      elsif new_position > num_items - 1
+        new_position = num_items - 1
+      end
+  
+      return true if new_position == position
+  
+      if new_position > orig_position
+        update_position(orig_position + 1, new_position, :decrement)
+      else
+        update_position(new_position, orig_position - 1, :increment)
+      end
     end
-
-    return true if new_position == position
-
-    if new_position > orig_position
-      update_position(orig_position + 1, new_position, :decrement)
-    else
-      update_position(new_position, orig_position - 1, :increment)
-    end
-
-    update(position: new_position)
   end
 
+  private
+  
   def update_position(start_idx, end_idx, update_type = :decrement)
     return if start_idx > siblings.length - 1 && start_idx < 0
-    
     raise ArgumentError, 'Indices must be integers' unless start_idx.is_a?(Integer) && end_idx.is_a?(Integer)
     raise 'Invalid update type' unless %i[decrement increment].include? update_type
     raise 'Start index cannot be larger than end index' if start_idx > end_idx
     siblings.where(position: start_idx..end_idx).each do |sibling|
-      update_type == :decrement ? sibling.decrement(:position).save : sibling.increment(:position).save
+      update_type == :decrement ? sibling.decrement(:position).save! : sibling.increment(:position).save!
     end
   end
 end
