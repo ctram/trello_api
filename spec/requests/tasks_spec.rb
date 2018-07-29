fb196c50360f1c38b3bb2bce4a9daf2ea9e89404
Chris Tram <chris.tram@gmail.com>
2018-07-29 16:18:23 -0700

    add shared examples for tasks

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 require_relative '../../app/models/task'
 require_relative '../shared/examples/CRUD_actions'
+require_relative '../shared/examples/update_position'
 
 RSpec.describe 'Tasks', type: :request do
-  include_examples 'CRUD actions', Task, :column
+  describe 'CRUD actions' do
+    include_examples 'CRUD actions', Task, :column
+  end
+  describe 'update position' do
+    include_examples 'update position', Task, :column
+  end
 end
